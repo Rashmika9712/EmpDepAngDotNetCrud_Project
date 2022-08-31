6a74a9d1b2ef9501dcba5c3fd8628e3e3f274572
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2022-08-31 22:55:34 +0530

    first commit

---
+++ EmpDepAngDotNetCrud/testone/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testone.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string DepartmentName { get; set; }
+    }
+}
