bb6df1651469d57e955e0ee25d347736567256d0
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2022-09-03 18:58:44 +0530

    main

--- UIAngular/src/app/department/show-dep/show-dep.component.ts
+++ UIAngular/src/app/department/show-dep/show-dep.component.ts
@@ -10,7 +10,10 @@ export class ShowDepComponent implements OnInit {
 
   constructor(private service: SharedService) { }
 
-  DepartmentList: any=[];
+  DepartmentList: any=[];  
+  ModelTitle:string = "";
+  ActivateAddEditDepComp:boolean = false;
+  dep:any ="";
 
   ngOnInit(): void {
     this.refreshDepList();
@@ -22,4 +25,32 @@ export class ShowDepComponent implements OnInit {
     });
   }
 
+  addClick(){
+    this.dep = {
+      DepartmentId:0,
+      DepartmentName:"" 
+    }
+    this.ModelTitle = "Add Department";
+    this.ActivateAddEditDepComp = true;
+  }
+
+  editClick(item:any){
+    this.dep = item;
+    this.ModelTitle = "Edit Department";
+    this.ActivateAddEditDepComp = true;
+  }
+
+  deleteClick(item:any){
+    if(confirm('Are you sure?')){
+      this.service.deleteDepartment(item.DepartmentId).subscribe(data =>{
+        alert(data.toString());
+        this.refreshDepList();
+      });
+    }
+  }
+
+  closeClick(){
+    this.ActivateAddEditDepComp = false;
+    this.refreshDepList();
+  } 
 }
